b926862cff5a5c5a5928d45820500fd0843b87c4
gzapodea <gzapodea@cisco.com>
2021-01-13 09:23:52 -0800

    initial commit

--- pandemic_proximity_call.py
+++ pandemic_proximity_call.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 
-Copyright (c) 2020 Cisco and/or its affiliates.
+Copyright (c) 2021 Cisco and/or its affiliates.
 
 This software is licensed to you under the terms of the Cisco Sample
 Code License, Version 1.1 (the "License"). You may obtain a copy of the
@@ -22,24 +22,86 @@ or implied.
 __author__ = "Gabriel Zapodeanu TME, ENB"
 __email__ = "gzapodea@cisco.com"
 __version__ = "0.1.0"
-__copyright__ = "Copyright (c) 2020 Cisco and/or its affiliates."
+__copyright__ = "Copyright (c) 2021 Cisco and/or its affiliates."
 __license__ = "Cisco Sample Code License, Version 1.1"
 
 
+import requests
 import urllib3
+import datetime
 import json
-import os
 import time
-
+import logging
 
 from urllib3.exceptions import InsecureRequestWarning  # for insecure https warnings
+from requests.auth import HTTPBasicAuth  # for Basic Auth
+from datetime import datetime
 
-
-os.environ['TZ'] = 'America/Los_Angeles'  # define the timezone for PST
-time.tzset()  # adjust the timezone, more info https://help.pythonanywhere.com/pages/SettingTheTimezone/
+from config import DNAC_URL, DNAC_PASS, DNAC_USER
+from config import EVENT_ID
+from config import SUBSCRIPTION_NAME
+from config import WEBHOOK_URL
 
 urllib3.disable_warnings(InsecureRequestWarning)  # disable insecure https warnings
 
-from config import DNAC_URL, DNAC_USER, DNAC_PASS
+DNAC_AUTH = HTTPBasicAuth(DNAC_USER, DNAC_PASS)
 
 
+
+def pprint(json_data):
+    """
+    Pretty print JSON formatted data
+    :param json_data: data to pretty print
+    :return:
+    """
+    print(json.dumps(json_data, indent=4, separators=(' , ', ' : ')))
+
+
+def get_epoch_time(timestamp):
+    """
+    This function will return the epoch time for the {timestamp}, UTC time format, for current time
+    :param timestamp: timestamp in UTC format or none
+    :return: epoch time including msec
+    """
+    epoch = time.time()*1000
+    return int(epoch)
+
+
+def get_dnac_jwt_token(dnac_auth):
+    """
+    Create the authorization token required to access DNA C
+    Call to Cisco DNA Center - /api/system/v1/auth/login
+    :param dnac_auth - Cisco DNA Center Basic Auth string
+    :return: Cisco DNA Center JWT token
+    """
+    url = DNAC_URL + '/dna/system/api/v1/auth/token'
+    header = {'content-type': 'application/json'}
+    response = requests.post(url, auth=dnac_auth, headers=header, verify=False)
+    dnac_jwt_token = response.json()['Token']
+    return dnac_jwt_token
+
+
+def main():
+    """
+
+    """
+
+    # logging, debug level, to file {application_run.log}
+    logging.basicConfig(
+        filename='application_run.log',
+        level=logging.DEBUG,
+        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    current_time = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('\n"pandemic_proximity_call.py" App Run Start, ', current_time)
+
+    # obtain a Cisco DNA Center auth token
+    dnac_auth = get_dnac_jwt_token(DNAC_AUTH)
+
+    current_time = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('\n"pandemic_proximity_call.py" App Run End, ', current_time)
+
+
+if __name__ == '__main__':
+    main()
